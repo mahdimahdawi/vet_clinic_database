3fe9691ab48988456d53f8ef0cba5eee4fa49db2
Mahdi Mahdawi <59498020+mahdimahdawi@users.noreply.github.com>
2022-10-26 20:07:46 +0430

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -9,6 +9,40 @@ CREATE TABLE animals (
     weight_kg decimal
 );
 
+/* Day 2 Project */
 ALTER TABLE animals
 ADD COLUMN species VARCHAR (32);
 
+/* Day 3 Project */
+CREATE TABLE owners(
+    id INT GENERATED ALWAYS AS INDENTITY PRIMARY KEY,
+    full_name VARCHAR(64),
+    age INT
+    );
+    
+CREATE TABLE species(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(32)
+    );
+    
+ ALTER TABLE animals
+ ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY;
+ ALTER TABLE animals ADD PRIMARY KEY(id);
+ ALTER TABLE animals DROP species;
+ 
+ ALTER TABLE animals ADD COLUMN species_id INT;
+ ALTER TABLE animals
+ ADD CONSTRAINT CONS
+ FOREIGN KEY(species_id)
+ REFERENCES species(id);
+ 
+ 
+ ALTER TABLE animals ADD COLUMN owner_id INT;
+ ALTER TABLE animals
+ ADD CONSTRAINT owner_fk
+ FOREIGN KEY(owner_id)
+ REFERENCES owners(id);
+ 
+ 
+ 
+    
