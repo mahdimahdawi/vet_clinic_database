2b661752f5ff44f9d0a66e0fc2ad1015cc53de64
Mahdi Mahdawi <59498020+mahdimahdawi@users.noreply.github.com>
2022-10-26 11:23:25 +0430

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -7,3 +7,29 @@ SELECT name, escape_attempts FROM animals WHERE wieght_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE wieght_kg >= 10.4 AND wieght_kg <= 17.3;
+
+/* Day 2 Project */
+BEGIN TRANSACTION;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+
+BEGIN TRANSACTION;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE name NOT LIKE '%mon';
+COMMIT;
+SELECT * FROM animals;
+
+BEGIN TRANSACTION;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN TRANSACTION;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT SP1;
+UPDATE animals SET wieght_kg = wieght_kg * -1;
+ROLLBACK TO SP1;
+UPDATE animals SET wieght_kg = wieght_kg * -1 WHERE wieght_kg < 0;
+COMMIT;
+SELECT * FROM animals;
