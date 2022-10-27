5aeec90723db1bf086bb52d854a36828d1745c54
Mahdi Mahdawi <59498020+mahdimahdawi@users.noreply.github.com>
2022-10-27 12:28:51 +0430

    REQUIRED CHANGES DONE

--- queries.sql
+++ queries.sql
@@ -46,7 +46,7 @@ SELECT name FROM animals A
 JOIN owners O ON O.id = A.owner_id AND O.full_name = 'Melody Pond';
 
 SELECT * FROM animals A
-JOIN species S ON A.species_id = S.id AND A.species_id = 1;
+JOIN species S ON A.species_id = S.id AND A.name = 'Pokemon';
 
 SELECT owners.full_name, animals.name FROM animals
 FULL JOIN owners ON owners.id = animals.owner_id;
@@ -55,10 +55,10 @@ SELECT COUNT(species_id) FROM animals
 FULL JOIN species ON species.id = animals.species_id GROUP BY species_id;
 
 SELECT name FROM animals
-RIGHT JOIN owners ON owners.id = animals.owner_id AND owner_id = 2 AND species_id = 2;
+RIGHT JOIN owners ON owners.id = animals.owner_id AND owners.full_name = 'Jennifer';
 
 SELECT name FROM animals
-JOIN owners ON owners.id = animals.owner_id AND escape_attempts = 0 AND owner_id = 5;
+JOIN owners ON owners.id = animals.owner_id AND escape_attempts = 0 AND owners.full_name = 'Dean Winchester';
 
 SELECT owners.full_name,  COUNT(owner_id) FROM animals
 RIGHT JOIN owners ON owners.id = animals.owner_id GROUP BY owners.full_name;
